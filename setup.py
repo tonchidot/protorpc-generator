74f76c8247a2815123a9dc8b22bf8a4128c8b1e2
KAWACHI Takashi <kawachi@tonchidot.com>
2011-07-04 18:42:19 +0900

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(name='protorpc-generator',
+    version='1.0',
+    description='ProtoRPC generator',
+    package_dir = {'': 'bin'},
+    packages=['rpc_generators'],
+    scripts=['bin/protoc-gen-rpc', 'bin/proto2rpc'],
+    install_requires=[
+        'protobuf>=2.3.0',
+    ],
+    )
