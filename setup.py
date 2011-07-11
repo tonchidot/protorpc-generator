1d517ccbe180e2a4df217214921ff32bde0d11fb
Julien Cayzac <julien.cayzac@gmail.com>
2011-07-11 18:32:04 +0900

    Code cleanup, asi-http bug fix, and proper licensing/copyright info

--- setup.py
+++ setup.py
@@ -1,15 +1,46 @@
 #!/usr/bin/env python
-
+# coding=UTF-8
+#
+# Copyright 2011 Tonchidot Corporation. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    1. Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+#
+#    2. Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS "AS IS" AND ANY EXPRESS
+# OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+# NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# The views and conclusions contained in the software and documentation are
+# those of the authors and should not be interpreted as representing official
+# policies, either expressed or implied, of the copyright holders.
+#
+# author: Takashi Kawachi https://github.com/tkawachi
 from distutils.core import setup
 
 
 setup(name='protorpc-generator',
-    version='1.0',
-    description='ProtoRPC generator',
-    package_dir = {'': 'bin'},
+    version='0.1',
+    description="Protoc plugin to generate mobile RPC services for GAE",
+    package_dir={'': 'bin'},
     packages=['rpc_generators'],
-    scripts=['bin/protoc-gen-rpc', 'bin/proto2rpc'],
+    scripts=['bin/protoc-gen-rpc'],
     install_requires=[
-        'protobuf>=2.4.1',
-    ],
-    )
+        'protobuf>=2.4.1'
+    ]
+)
+
+# vim: set fileencodings=utf-8 tabstop=4 softtabstop=4 shiftwidth=4 expandtab
